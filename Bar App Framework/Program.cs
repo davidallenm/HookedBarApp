db40c5a9909af1065830623663bcf6db9fd5dbde
David Mcdowell <davidallenm97@gmail.com>
2018-11-25 17:12:25 -0600

    Initial Commit

---
+++ Bar App Framework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bar_App_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
